897b33a81a4adea7a9b07cfd5ea739cba15e76d4
NAIM BISWAS <naimbiswas@NAIMs-MacBook-Pro.local>
2024-07-16 00:15:53 +0200

    remove secrect keys

--- youtube_summarizer/settings.py
+++ youtube_summarizer/settings.py
@@ -18,11 +18,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["youtube-summarizer-fbe026bb7c16.herokuapp.com", "localhost"]
