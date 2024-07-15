de0063dc3493020a93690b25ec08b8942f10169d
NAIM BISWAS <naimbiswas@NAIMs-MacBook-Pro.local>
2024-07-16 00:14:45 +0200

    Second commit

--- youtube_summarizer/settings.py
+++ youtube_summarizer/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# ADD keys here.
-
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
